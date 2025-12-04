45cfe051d6f5e33816ed8811592b3cffec67ead6
Preben Eriksen <plemsoft@gmail.com>
2025-12-04 11:40:36 +0100

    * PE: Support x and z offsets for wicked particle previde ( discord issue ).

--- GameGuru Core/GameGuru/Imgui/imgui_gg_dx11.cpp
+++ GameGuru Core/GameGuru/Imgui/imgui_gg_dx11.cpp
@@ -69,6 +69,8 @@ int ImGuiStatusBar_Size = 0;
 bool bPreviewWPE = false;
 uint32_t PreviewWPERoot = 0;
 float fPreviewYOffset = 0;
+float fPreviewXOffset = 0;
+float fPreviewZOffset = 0;
 
 preferences pref;
 bool g_bEnableAutoFlattenSystem = true;
@@ -8421,9 +8423,41 @@ int DisplayLuaDescription(entityeleproftype *tmpeleprof)
 				if (stricmp (tmpeleprof->PropertiesVariable.Variable[i], "ChaseModes") == NULL) pTooltipForIntegerSlider = "The first three modes are slow walkers and the last two are fast walkers";
 				cstr id = cstr("##") + tmpeleprof->PropertiesVariable.VariableScript/*tmpeleprof->name_s*/ + cstr(tmpeleprof->PropertiesVariable.Variable[i]);
 
+				bool bwpeyoffet = false;
+				bool bwpexoffet = false;
+				bool bwpezoffet = false;
+
+				if (bwpefile)
+				{
+					if (pestrcasestr(tmpeleprof->PropertiesVariable.Variable[i], "offsety"))
+					{
+						bwpeyoffet = true;
+						fPreviewYOffset = 0;
+						fPreviewXOffset = 0;
+						fPreviewZOffset = 0;
+					}
+					if (pestrcasestr(tmpeleprof->PropertiesVariable.Variable[i], "offsetx"))
+					{
+						bwpexoffet = true;
+					}
+					if (pestrcasestr(tmpeleprof->PropertiesVariable.Variable[i], "offsetz"))
+					{
+						bwpezoffet = true;
+					}
+				}
+
 				// strange condition to enable correct integer slider - from can be zero just fine
 				//if (tmpeleprof->PropertiesVariable.VariableValueFrom[i] != 0 && tmpeleprof->PropertiesVariable.VariableValueTo[i] != 0 && tmpeleprof->PropertiesVariable.VariableValueTo[i] > tmpeleprof->PropertiesVariable.VariableValueFrom[i])
-				if (tmpeleprof->PropertiesVariable.VariableValueTo[i] != 0 && tmpeleprof->PropertiesVariable.VariableValueTo[i] > tmpeleprof->PropertiesVariable.VariableValueFrom[i])
+				if (bwpefile && (bwpeyoffet || bwpexoffet || bwpezoffet))
+				{
+					if (ImGui::MaxSliderInputInt(id.Get(), &tmpint, -100.0F, 100.0f, pTooltipForIntegerSlider))
+					{
+						sprintf(tmp, "%d", tmpint);
+						strcpy(tmpeleprof->PropertiesVariable.VariableValue[i], tmp);
+						bUpdateMainString = true;
+					}
+				}
+				else if (tmpeleprof->PropertiesVariable.VariableValueTo[i] != 0 && tmpeleprof->PropertiesVariable.VariableValueTo[i] > tmpeleprof->PropertiesVariable.VariableValueFrom[i])
 				{
 					if (ImGui::MaxSliderInputInt(id.Get(), &tmpint, (int)tmpeleprof->PropertiesVariable.VariableValueFrom[i], (int)tmpeleprof->PropertiesVariable.VariableValueTo[i], pTooltipForIntegerSlider))
 					{
@@ -8441,12 +8475,20 @@ int DisplayLuaDescription(entityeleproftype *tmpeleprof)
 						bUpdateMainString = true;
 					}
 				}
+
 				if (bwpefile)
 				{
-					if (pestrcasestr(tmpeleprof->PropertiesVariable.Variable[i], "offsety"))
+					if (bwpeyoffet)
 					{
-						bwpeyoffet = true;
 						fPreviewYOffset = tmpint;
+					}
+					if (bwpexoffet)
+					{
+						fPreviewXOffset = tmpint;
+					}
+					if (bwpezoffet)
+					{
+						fPreviewZOffset = tmpint;
 					}
 				}
 
@@ -8456,9 +8498,6 @@ int DisplayLuaDescription(entityeleproftype *tmpeleprof)
 		}
 	}
 	
-	if(!bwpeyoffet)
-		fPreviewYOffset = 0;
-
 	//Update soundset4_s when we have changes.
 	if (bUpdateMainString) 
 	{
