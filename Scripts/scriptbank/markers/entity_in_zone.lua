c09c254ba003ac536d9cdcebd789a1525fa7fea0
Steve <95015657+Necrym59@users.noreply.github.com>
2026-02-12 16:11:33 +1100

    Update entity_in_zone.lua
    * NC: Update entity_in_zone behavior - optional self distruct

--- Scripts/scriptbank/markers/entity_in_zone.lua
+++ Scripts/scriptbank/markers/entity_in_zone.lua
@@ -1,11 +1,12 @@
 -- LUA Script - precede every function and global member with lowercase name of script + '_main'
--- Entity In Zone v10 by Necrym59 and Lee
--- DESCRIPTION: If entity of selected type and allegiance enters the zone, will activate linked or IfUsed entities then destroys this zone.
+-- Entity In Zone v11 by Necrym59 and Lee
+-- DESCRIPTION: If entity of selected type and allegiance enters the zone, will activate linked or IfUsed entities then can destroy zone.
 -- DESCRIPTION: [@ENTITY_TYPE=1(1=Active Object Only, 2=Active Character Only, 3=Active Non-Character Only, 4=Non-Static Objects)]
 -- DESCRIPTION: [@ALLEGIANCE=3(0=Enemy, 1=Ally, 2=Neutral, 3=None, 4=Any)]
 -- DESCRIPTION: [NOTIFICATION$="Entity in zone"]
 -- DESCRIPTION: [ZONEHEIGHT=100(0,1000)]
 -- DESCRIPTION: [SpawnAtStart!=1] if unchecked use a switch or other trigger to spawn this zone
+-- DESCRIPTION: [SelfDestroy!=1] if checked will destroy zone after activation/detection
 -- DESCRIPTION: Plays <Sound0> when triggered.
 
 local entityinzone 		= {}
@@ -13,6 +14,7 @@ local entity_type 		= {}
 local allegiance 		= {}
 local zoneheight		= {}
 local spawnatstart		= {}
+local selfdestroy		= {}
 
 local EntityID			= {}
 local EntityAL			= {}
@@ -21,12 +23,13 @@ local played 			= {}
 local doonce			= {}
 local status			= {}
 	
-function entity_in_zone_properties(e, entity_type, allegiance, notification, zoneheight, spawnatstart)
+function entity_in_zone_properties(e, entity_type, allegiance, notification, zoneheight, spawnatstart, selfdestroy)
 	entityinzone[e].entity_type = entity_type
 	entityinzone[e].allegiance = allegiance
 	entityinzone[e].notification = notification
 	entityinzone[e].zoneheight = zoneheight
 	entityinzone[e].spawnatstart = spawnatstart
+	entityinzone[e].selfdestroy = selfdestroy
 end
  
 function entity_in_zone_init(e)
@@ -36,6 +39,7 @@ function entity_in_zone_init(e)
 	entityinzone[e].notification = "Entity in zone"
 	entityinzone[e].zoneheight = 100
 	entityinzone[e].spawnatstart = 1
+	entityinzone[e].selfdestroy = 1
 	
 	status[e] = "init"
 	played[e] = 0
@@ -119,7 +123,9 @@ function entity_in_zone_main(e)
 			end
 		end	
 	end
-	if g_Time > wait[e] then		
-		Destroy(e)
+	if g_Time > wait[e] then
+		if entityinzone[e].selfdestroy ==  1 then Destroy(e) end
+		played[e] = 0
+		doonce[e] = 0
 	end
 end
