01444597faaffa0e4e03abff11f5717185602602
Steve <95015657+Necrym59@users.noreply.github.com>
2026-04-17 15:47:12 +1000

    Update dispenser.lua
    * NC: Update - dispenser behavior - internal fix

--- Scripts/scriptbank/objects/dispenser.lua
+++ Scripts/scriptbank/objects/dispenser.lua
@@ -46,13 +46,13 @@ local playonce	= {}
 
 function dispenser_properties(e, use_range, use_prompt, dispense_rate, dispense_max, dispense_to, user_global, when_empty, dispensing_animation, prompt_display, item_highlight, highlight_icon_imagefile)
     dispenser[e].use_range = use_range
-    dispenser[e].use_prompt = use_prompt	
+    dispenser[e].use_prompt = use_prompt
     dispenser[e].dispense_rate = dispense_rate
     dispenser[e].dispense_max = dispense_max
     dispenser[e].dispense_to = dispense_to
     dispenser[e].user_global = user_global
     dispenser[e].when_empty = when_empty
-    dispenser[e].dispensing_animation = "=" .. tostring(dispensing_animation)	
+    dispenser[e].dispensing_animation = "=" .. tostring(dispensing_animation)
     dispenser[e].prompt_display = prompt_display
     dispenser[e].item_highlight = item_highlight
     dispenser[e].highlight_icon = highlight_icon_imagefile
@@ -61,13 +61,13 @@ end
 function dispenser_init(e)
     dispenser[e] = {}
     dispenser[e].use_range = 80
-    dispenser[e].use_prompt = "Hold E to use"	
+    dispenser[e].use_prompt = "Hold E to use"
     dispenser[e].dispense_rate = 1
     dispenser[e].dispense_max = 50
     dispenser[e].dispense_to = dispense_to
     dispenser[e].user_global = user_global
-    dispenser[e].when_empty = 1	
-	dispenser[e].dispensing_animation = ""	
+    dispenser[e].when_empty = 1
+	dispenser[e].dispensing_animation = ""
     dispenser[e].prompt_display = prompt_display or 1
     dispenser[e].item_highlight = 0
     dispenser[e].highlight_icon = "imagebank\\icons\\hand.png"
@@ -77,8 +77,8 @@ function dispenser_init(e)
 	dispense_timer[e] = math.huge
 	hl_icon[e] = 0
 	hl_imgwidth[e] = 0
-	hl_imgheight[e] = 0	
-	currentvalue[e] = 0	
+	hl_imgheight[e] = 0
+	currentvalue[e] = 0
 	g_tEnt = 0
 	tEnt[e] = 0
 	playonce[e] =0
@@ -99,7 +99,7 @@ function dispenser_main(e)
 		end
 		if dispenser[e].user_global ~= "" then
 			if _G["g_UserGlobal['"..dispenser[e].user_global.."']"] ~= nil then currentvalue[e] = _G["g_UserGlobal['"..dispenser[e].user_global.."']"] end
-		end		
+		end
 		dispense_timer[e] = g_Time + 100
 		status[e] = "endinit"
 	end
@@ -113,6 +113,8 @@ function dispenser_main(e)
 	end
 
 	if PlayerDist < dispenser[e].use_range and tEnt[e] == e then
+		if dispenser[e].prompt_display == 1 then TextCenterOnX(50,53,1, dispenser[e].use_prompt) end
+		if dispenser[e].prompt_display == 2 then Prompt(dispenser[e].use_prompt) end
 		if dispenser[e].user_global ~= "" then
 			if _G["g_UserGlobal['"..dispenser[e].user_global.."']"] ~= nil then currentvalue[e] = _G["g_UserGlobal['"..dispenser[e].user_global.."']"] end
 		end
@@ -120,7 +122,7 @@ function dispenser_main(e)
 			if dispenser[e].dispensing_animation ~= "" then
 				SetAnimationName(e,dispenser[e].dispensing_animation)
 				LoopAnimation(e)
-			end		
+			end
 			LoopSound(e,0)
 			dispensed_level[e] = dispensed_level[e] + dispenser[e].dispense_rate
 			dispensed_total[e] = dispensed_total[e] + dispensed_level[e]
@@ -128,10 +130,10 @@ function dispenser_main(e)
 			if dispenser[e].dispense_to == 1 then -- Health Attribute
 				SetPlayerHealth(g_PlayerHealth + dispensed_level[e])
 				if g_PlayerHealth > g_PlayerStartStrength then g_PlayerHealth = g_PlayerStartStrength end
-				SetPlayerHealthCore(g_PlayerHealth)					
+				SetPlayerHealthCore(g_PlayerHealth)
 			end
 			if dispenser[e].dispense_to == 2 then -- User Global Attribute
-				if dispenser[e].user_global ~= "" then					
+				if dispenser[e].user_global ~= "" then
 					_G["g_UserGlobal['"..dispenser[e].user_global.."']"] = currentvalue[e]
 				end
 			end
@@ -158,8 +160,8 @@ function dispenser_main(e)
 		end
 		if dispenser[e].when_empty == 3 then
 			ActivateIfUsed(e)
-			PerformLogicConnections(e)		
+			PerformLogicConnections(e)
 			SwitchScript(e,"no_behavior_selected.lua")
 		end
-	end	
+	end
 end
