33ba9f5878dbaefe6f83151b2ede155486315414
Steve <95015657+Necrym59@users.noreply.github.com>
2026-04-17 18:32:55 +1000

    Update communicator.lua
    * NC: Update - communicator behavior - internal fixes

--- Scripts/scriptbank/objects/communicator.lua
+++ Scripts/scriptbank/objects/communicator.lua
@@ -1,19 +1,22 @@
 -- LUA Script - precede every function and global member with lowercase name of script + '_main'
--- Communicator v3 by Necrym59
+-- Communicator v4 by Necrym59
 -- DESCRIPTION: Creates an audible communicator device that can be answered by the player.
 -- DESCRIPTION: Attach to an object and activate from a switch or zone.
 -- DESCRIPTION: [USE_RANGE=80(1,200)] range player can interact with
 -- DESCRIPTION: [USE_PROMPT$="Press E to answer and to end-call"]
+-- DESCRIPTION: [@USE_MODE=1(1=None, 2=Hide Communicator, 3=Freeze Player, 4=Hide Communicator + Freeze Player)]
 -- DESCRIPTION: [@USE_TRIGGER=1(1=Off, 2=Call Answer, 3=Call End, 4=Answer+End)]
 -- DESCRIPTION: [TONE_VOLUME=100(0,100)] call tone sound volume
+-- DESCRIPTION: [PICKUP_VOLUME=100(0,100)] call pickup sound volume
 -- DESCRIPTION: [CALL_VOLUME=100(0,100)] call sound volume
 -- DESCRIPTION: [END_VOLUME=100(0,100)] end sound volume
 -- DESCRIPTION: [@PROMPT_DISPLAY=1(1=Local,2=Screen)]
 -- DESCRIPTION: [@ITEM_HIGHLIGHT=0(0=None,1=Shape,2=Outline,3=Icon)] Use emmisive color for shape option
 -- DESCRIPTION: [HIGHLIGHT_ICON_IMAGEFILE$="imagebank\\icons\\pickup.png"]
 -- DESCRIPTION: <Sound0> Tone loop
--- DESCRIPTION: <Sound1> Call sound
--- DESCRIPTION: <Sound2> End sound
+-- DESCRIPTION: <Sound1> Pickup sound
+-- DESCRIPTION: <Sound2> Communication sound
+-- DESCRIPTION: <Sound3> End sound
 
 local module_misclib = require "scriptbank\\module_misclib"
 g_tEnt = {}
@@ -21,8 +24,10 @@ g_tEnt = {}
 local communicator		= {}
 local use_range			= {}
 local use_prompt		= {}
+local use_mode			= {}
 local use_trigger		= {}
 local tone_volume		= {}
+local pickup_volume		= {}
 local call_volume		= {}
 local end_volume		= {}
 local prompt_display 	= {}
@@ -37,13 +42,18 @@ local tEnt				= {}
 local keypressed		= {}
 local doonce			= {}
 local keypausetimer		= {}
+local freezex			= {}
+local freezey			= {}
+local freezez			= {}
 
 
-function communicator_properties(e, use_range, use_prompt, use_trigger, tone_volume, call_volume, end_volume, prompt_display, item_highlight, highlight_icon_imagefile)
+function communicator_properties(e, use_range, use_prompt, use_mode, use_trigger, tone_volume, pickup_volume, call_volume, end_volume, prompt_display, item_highlight, highlight_icon_imagefile)
 	communicator[e].use_range = use_range
 	communicator[e].use_prompt = use_prompt
+	communicator[e].use_mode = use_mode
 	communicator[e].use_trigger = use_trigger
 	communicator[e].tone_volume = tone_volume
+	communicator[e].pickup_volume = pickup_volume
 	communicator[e].call_volume = call_volume
 	communicator[e].end_volume = end_volume	
 	communicator[e].prompt_display = prompt_display
@@ -55,8 +65,10 @@ function communicator_init(e)
 	communicator[e] = {}
 	communicator[e].use_range = 80
 	communicator[e].use_prompt = "Press E to answer and to end-call"
+	communicator[e].use_mode = 1
 	communicator[e].use_trigger = 1	
 	communicator[e].tone_volume = 100
+	communicator[e].pickup_volume = 100	
 	communicator[e].call_volume = 100
 	communicator[e].end_volume = 100
 	communicator[e].prompt_display = 1
@@ -71,6 +83,10 @@ function communicator_init(e)
 	keypressed[e] = 0
 	reset_timer[e] = 0
 	doonce[e] = 0
+	freezex[e] = 0
+	freezey[e] = 0
+	freezez[e] = 0
+	freezeangy[e] = 0
 	keypausetimer[e] = math.huge
 	status[e] = "init"	
 end
@@ -90,10 +106,9 @@ function communicator_main(e)
 		status[e] = "endinit"
 		SetActivated(e,0)
 	end
-	
+
 	if g_Entity[e]['activated'] == 1 then
-		local PlayerDist = GetPlayerDistance(e)
-		
+
 		if doonce[e] == 0 then
 			keypausetimer[e] = math.huge
 			doonce[e] = 1
@@ -106,13 +121,13 @@ function communicator_main(e)
 			StopSound(e,0)
 		end				
 				
-		if PlayerDist < communicator[e].use_range then
+		if GetPlayerDistance(e) < communicator[e].use_range then
 			--pinpoint select object--
 			module_misclib.pinpoint(e,communicator[e].use_range,communicator[e].item_highlight,hl_icon[e])
 			tEnt[e] = g_tEnt
 			--end pinpoint select object--
 		end			
-		if PlayerDist < communicator[e].use_range and tEnt[e] == e then
+		if GetPlayerDistance(e) < communicator[e].use_range and tEnt[e] == e then
 			if keypressed[e] == 0 then
 				if communicator[e].prompt_display == 1 then PromptLocal(e,communicator[e].use_prompt) end
 				if communicator[e].prompt_display == 2 then Prompt(communicator[e].use_prompt) end
@@ -120,33 +135,54 @@ function communicator_main(e)
 			if g_KeyPressE == 1 and keypressed[e] == 0 then
 				keypausetimer[e] = g_Time + 1000
 				keypressed[e] = 1				
-				StopSound(e,0)
+				StopSound(e,0)				
 				SetSound(e,1)
-				SetSoundVolume(communicator[e].call_volume)
-				PlaySound(e,1)
-				if communicator[e].use_trigger == 2 or communicator[e].use_trigger == 4 then 
+				SetSoundVolume(communicator[e].pickup_volume)
+				PlaySound(e,1)				
+				if communicator[e].use_mode == 2 or communicator[e].use_mode == 4 then
+					Hide(e)
+				end
+				if communicator[e].use_mode == 3 or communicator[e].use_mode == 4 then
+					freezex[e] = g_PlayerPosX
+					freezey[e] = g_PlayerPosY
+					freezez[e] = g_PlayerPosZ
+				end	
+				if communicator[e].use_trigger == 2 or communicator[e].use_trigger == 4 then
 					PerformLogicConnections(e)
 					ActivateIfUsed(e)
 				end
-			end
-			if keypressed[e] == 1 and GetSoundPlaying(e,1) == 0 then g_KeyPressE = 1 end
-			if g_KeyPressE == 1 and keypressed[e] == 1 and g_Time > keypausetimer[e] then
-				StopSound(e,0)
-				StopSound(e,1)
 				SetSound(e,2)
-				SetSoundVolume(communicator[e].end_volume)
+				SetSoundVolume(communicator[e].call_volume)
 				PlaySound(e,2)
+				status[e] = "communicating"
+			end			
+		end	
+		if status[e] == "communicating"	then
+			if communicator[e].use_mode == 3 or communicator[e].use_mode == 4 then
+				SetFreezePosition(freezex[e],freezey[e],freezez[e])	
+				TransportToFreezePositionOnly()
+			end 
+			if keypressed[e] == 1 and GetSoundPlaying(e,2) == 0 then g_KeyPressE = 1 end
+			if g_KeyPressE == 1 and keypressed[e] == 1 and g_Time > keypausetimer[e] then
+				StopSound(e,2)
+				SetSound(e,3)
+				SetSoundVolume(communicator[e].end_volume)
+				PlaySound(e,3)
+				if communicator[e].use_mode == 2 or communicator[e].use_mode == 4 then
+					Show(e)
+				end
 				if communicator[e].use_trigger == 3 or communicator[e].use_trigger == 4 then
 					PerformLogicConnections(e)
 					ActivateIfUsed(e)
-				end		
+				end				
 				keypausetimer[e] = math.huge
 				keypressed[e] = 0
 				doonce[e] = 0
-				SetActivated(e,0)				
+				status[e] = "endcall"
+				SetActivated(e,0)
 			end
 		end
-		if PlayerDist > communicator[e].use_range * 10 then
+		if GetPlayerDistance(e) > communicator[e].use_range * 10 then
 			StopSound(e,0)
 			SetActivated(e,0)		
 		end	
